49b3d4f3cae0099031728b4c3f9836d26b31a3be
ijimnun907 <ijimnun907@g.educaand.es>
2025-04-15 22:05:53 +0200

    arreglado fallo de integridad, email de usuario unico

---
+++ migrations/Version20250415200456.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20250415200456 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql(<<<'SQL'
+            CREATE UNIQUE INDEX UNIQ_2265B05DE7927C74 ON usuario (email)
+        SQL);
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql(<<<'SQL'
+            DROP INDEX UNIQ_2265B05DE7927C74 ON usuario
+        SQL);
+    }
+}
