f81e46a744b4e3812ddb08afa3e31d83df3c638f
ijimnun907 <ijimnun907@g.educaand.es>
2025-04-27 21:26:38 +0200

    Modificar crear y eliminar un usuario

--- src/Controller/UsuarioController.php
+++ src/Controller/UsuarioController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\Usuario;
+use App\Form\RegistrationType;
 use App\Form\UsuarioType;
 use App\Repository\UsuarioRepository;
 use Doctrine\ORM\EntityManagerInterface;
@@ -14,6 +15,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class UsuarioController extends AbstractController
 {
+    // Este es para personas nuevas
     #[Route('/registro', name: 'app_registro')]
     public function registro(
         Request $request,
@@ -22,7 +24,7 @@ class UsuarioController extends AbstractController
     ): Response
     {
         $usuario = new Usuario();
-        $form = $this->createForm(UsuarioType::class, $usuario);
+        $form = $this->createForm(RegistrationType::class, $usuario);
 
         $form->handleRequest($request);
 
@@ -44,6 +46,71 @@ class UsuarioController extends AbstractController
 
         return $this->render('usuario/registro.html.twig', [
             'form' => $form->createView(),
+        ]);
+    }
+
+    // Este es solo para admins, igual que crear
+    #[Route('/usuario/modificar/{id}', name: 'usuario_modificar')]
+    public function modificar(Request $request, UsuarioRepository $usuarioRepository, Usuario $usuario) : Response
+    {
+        $form = $this->createForm(UsuarioType::class, $usuario);
+
+        $form->handleRequest($request);
+
+        $nuevo = $usuario->getId() === null;
+
+        if ($form->isSubmitted() && $form->isValid()){
+            try {
+                $usuarioRepository->save();
+                if ($nuevo){
+                    $this->addFlash('success', 'Usuario creado con éxito');
+                }
+                else {
+                    $this->addFlash('success', 'Usuario modificado con éxito');
+                }
+                return $this->redirectToRoute('modificar_ruta');
+            }
+            catch (\Exception $e){
+                $this->addFlash('error', 'No se han podido guardar los cambios');
+            }
+        }
+        return $this->render('usuario/modificar.html.twig', [
+            'form' => $form->createView(),
+            'usuario' => $usuario
+        ]);
+    }
+
+    #[Route('/usuario/nuevo', name: 'usuario_nuevo')]
+    public function nuevo(Request $request, UsuarioRepository $usuarioRepository, UserPasswordHasherInterface $passwordHasher) : Response
+    {
+        $usuario = new Usuario();
+        $hashedPassword = $passwordHasher->hashPassword(
+            $usuario,
+            'cambiarClave'
+        );
+        $usuario->setPassword($hashedPassword);
+        $usuarioRepository->add($usuario);
+
+        return $this->modificar($request, $usuarioRepository, $usuario);
+    }
+
+    #[Route('/usuario/eliminar/{id}', name: 'usuario_eliminar')]
+    public function eliminar(Request $request, UsuarioRepository $usuarioRepository, Usuario $usuario) : Response
+    {
+        if ($request->request->has('confirmar')){
+            try {
+                $usuarioRepository->remove($usuario);
+                $usuarioRepository->save();
+                $this->addFlash('success', 'Usuario eliminado con exito');
+                return $this->redirectToRoute('modificar_ruta');
+            }
+            catch (\Exception $e){
+                $this->addFlash('error', 'No se ha podido eliminar el usuario');
+            }
+        }
+
+        return $this->render('usuario/eliminar.html.twig', [
+            'usuario' => $usuario
         ]);
     }
 }
---
+++ src/Form/UsuarioType.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Usuario;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class UsuarioType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('username', TextType::class)
+            ->add('email', TextType::class)
+            ->add('telefono', TextType::class)
+            ->add('esAdministrador', CheckboxType::class, [
+                'label' => 'Es administrador',
+                'required' => false
+            ])
+            ->add('esCamarero', CheckboxType::class, [
+                'label' => 'Es camarero',
+                'required' => false
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Usuario::class,
+        ]);
+    }
+}
