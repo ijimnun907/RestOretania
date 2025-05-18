32b2b1c801317de1f311947d3c7fc666ce9a19e3
ijimnun907 <ijimnun907@g.educaand.es>
2025-05-18 23:11:47 +0200

    UniqueEntity hecho

--- src/Entity/Usuario.php
+++ src/Entity/Usuario.php
@@ -6,11 +6,13 @@ use App\Repository\UsuarioRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: UsuarioRepository::class)]
+#[UniqueEntity('email', message: 'Este email ya está registrado.')]
 class Usuario implements UserInterface, PasswordAuthenticatedUserInterface
 {
     #[ORM\Id]
