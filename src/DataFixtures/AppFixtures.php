96b14a07adb1927b8082ac1f581e793da4a993a8
ijimnun907 <ijimnun907@g.educaand.es>
2025-05-18 23:23:46 +0200

    Fixtures configurado para crear cuenta de administrador

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,16 +2,36 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Usuario;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class AppFixtures extends Fixture
 {
+    private UserPasswordHasherInterface $passwordHasher;
+
+    public function __construct(UserPasswordHasherInterface $passwordHasher)
+    {
+        $this->passwordHasher = $passwordHasher;
+    }
+
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $admin = new Usuario();
+        $admin->setUsername('admin');
+        $admin->setEmail('admin@restaurante.com');
+        $admin->setTelefono('123456789');
+        $admin->setEsAdministrador(true);
+        $admin->setEsCamarero(false);
 
+        $hashedPassword = $this->passwordHasher->hashPassword(
+            $admin,
+            'admin123'
+        );
+        $admin->setPassword($hashedPassword);
+
+        $manager->persist($admin);
         $manager->flush();
     }
 }
